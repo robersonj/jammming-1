bb14e646ee8e3b1cb276bebd060cc9a07239e487
DagmarTimmreck <DagmarTimmreck@users.noreply.github.com>
2017-09-18 11:06:54 +0200

    give PlayList state

--- src/components/PlayList/PlayList.js
+++ src/components/PlayList/PlayList.js
@@ -2,23 +2,30 @@ import React from 'react';
 import './PlayList.css';
 import TrackList from '../../components/TrackList/TrackList';
 
-function PlayList() {
-  const info = {
-    title: 'Tiny Dancer',
-    artist: 'Elton John',
-    album: 'Madman Across The Water',
-  };
-  const action = {
-    symbol: '-',
-  };
-  const infoList = [info, info, info];
-  return (
-    <div className="Playlist">
-      <input value="New Playlist" />
-      <TrackList infoList={infoList} action={action} />
-      <a className="Playlist-save">SAVE TO SPOTIFY</a>
-    </div>
-  );
+class PlayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tracks: [],
+    };
+  }
+  addTrack(info) {
+    this.setState({
+      tracks: this.state.tracks.concat(info),
+    });
+  }
+  render() {
+    const action = {
+      symbol: '-',
+    };
+    return (
+      <div className="Playlist">
+        <input value="New Playlist" />
+        <TrackList infoList={this.state.tracks} action={action} />
+        <a className="Playlist-save">SAVE TO SPOTIFY</a>
+      </div>
+    );
+  }
 }
 
 export default PlayList;
--- test/components/PlayList.spec.js
+++ test/components/PlayList.spec.js
@@ -13,8 +13,30 @@ describe('PlayList', () => {
       <a>SAVE TO SPOTIFY</a>,
     ])).to.equal(true);
   });
+  it('should start with an empty list', () => {
+    const wrapper = shallow(<PlayList />);
+    expect(wrapper.state('tracks')).to.eql([]);
+  });
+  it('adds tracks to the list', () => {
+    const info = {
+      title: 'Tiny Dancer',
+      artist: 'Elton John',
+      album: 'Madman Across The Water',
+    };
+    const wrapper = shallow(<PlayList />);
+    wrapper.instance().addTrack(info);
+    expect(wrapper.state('tracks')).to.eql([info]);
+  });
   it('should render some items', () => {
+    const info = {
+      title: 'Tiny Dancer',
+      artist: 'Elton John',
+      album: 'Madman Across The Water',
+    };
     const wrapper = mount(<PlayList />);
+    wrapper.instance().addTrack(info);
+    wrapper.instance().addTrack(info);
+    wrapper.instance().addTrack(info);
     expect(wrapper.find('li')).to.have.length(3);
   });
 });
